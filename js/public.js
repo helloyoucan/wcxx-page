32c08125077c8e32406fd33d7db1f887a8d3fd37
can <helloyoucan@163.com>
2017-06-06 11:56:31 +0800

    add pages

--- js/public.js
+++ js/public.js
@@ -4,33 +4,31 @@ var message = new MyMessage.message({
 	messageFontSize: "15px", //信息字体大小,默认为12px
 	showTime: 3000, //消失时间,默认为3000
 	align: "right", //显示的位置类型center,right,left
-});;
-(function() {
-	//loadiuing
-	$('body').append('<div id="loading"><div class="l-bg"></div><div class="l-img"></div><p class="l-message">操作中...</p></div>')
-})();;
-(function() {
-	//表格选择多条数据
-	var $table = $('.checked-table');
-	$table.children('thead').on('change', 'input[type="checkbox"]', function() {
-		$tdCheckbox = $table.find('td input[type="checkbox"]')
-		if($(this).is(':checked')) {
-			$tdCheckbox.prop('checked', true);
-		} else {
-			$tdCheckbox.prop('checked', false);
-		}
-	});
-	$table.children('tbody').on('change', 'input[type="checkbox"]', function() {
-		var $thCheckbox = $table.find('th input[type="checkbox"]'),
-			checkboxSum = $table.find('td input[type="checkbox"]').length;
-		checkedSum = $table.find('td input[type="checkbox"]:checked').length;
-		if(checkedSum < checkboxSum) {
-			$thCheckbox.prop('checked', false);
-		} else if(checkedSum = checkboxSum) {
-			$thCheckbox.prop('checked', true);
-		}
-	});
-})();
+});
+//loading
+$('body').append('<div id="loading"><div class="l-bg"></div><div class="l-img"></div><p class="l-message">操作中...</p></div>')
+
+//表格选择多条数据
+var $table = $('.checked-table');
+$table.children('thead').on('change', 'input[type="checkbox"]', function() {
+	$tdCheckbox = $table.find('td input[type="checkbox"]')
+	if($(this).is(':checked')) {
+		$tdCheckbox.prop('checked', true);
+	} else {
+		$tdCheckbox.prop('checked', false);
+	}
+});
+$table.children('tbody').on('change', 'input[type="checkbox"]', function() {
+	var $thCheckbox = $table.find('th input[type="checkbox"]'),
+		checkboxSum = $table.find('td input[type="checkbox"]').length;
+	checkedSum = $table.find('td input[type="checkbox"]:checked').length;
+	if(checkedSum < checkboxSum) {
+		$thCheckbox.prop('checked', false);
+	} else if(checkedSum = checkboxSum) {
+		$thCheckbox.prop('checked', true);
+	}
+});
+
 //loading面板
 var loading = {
 	show: function(message) {
@@ -74,4 +72,45 @@ $uf_f_l.on('click', 'a', function() {
 	var delUrl = $uf_f_l.attr('data-delUrl'),
 		fileId = $(this).attr('data-id');
 	//code
+	alert('删除文件')
 });
+
+/**
+ * mid-table表格的添加和删除一行
+ * templateId:模板的id
+ * $added_dom：追加html的地方
+ * delDom:删除的那块的html的标签
+ * */
+function mid_table_action(templateId, added_dom, delDom, fnBeforeAdd, fnAfterRemove) {
+	//添加表格的一行
+	$mid_table = $('.mid-table');
+	$mid_table.find('#mt-btn-add').on('click', function() {
+		if(fnBeforeAdd) {
+			var index = fnBeforeAdd();
+			var mt_tr_html = template(templateId, {
+				index: index
+			});
+		} else {
+			var mt_tr_html = template(templateId);
+		}
+		$('#' + templateId).parent(added_dom).append(mt_tr_html);
+	});
+	//删除
+	$mid_table.find('#mt-btn-del').on('click', function() {
+		var $checkeds = $mid_table.find('tbody input[type="checkbox"]:checked');
+		if($checkeds.length) {
+			$checkeds.each(function(index, Element) {
+				$(this).parents(delDom).remove();
+				if(fnAfterRemove) {
+					fnAfterRemove();
+				}
+			});
+		} else {
+			message.add("请先选择一条或多条数据", "warning")
+		}
+
+		if(!$mid_table.find('td input[type="checkbox"]').length) {
+			$mid_table.find('th input[type="checkbox"]').prop('checked', false);
+		}
+	});
+}
